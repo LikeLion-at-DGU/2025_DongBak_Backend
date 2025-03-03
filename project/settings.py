a3b394fdefafed39b6c5b29a54520b7488e4896c
che-hyeon <ghkdcogus03@naver.com>
2025-03-03 15:41:19 +0900

    Feat: CSRF 주소 slash 추가

--- project/settings.py
+++ project/settings.py
@@ -161,8 +161,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://dongbak2025.shop",  
     "http://dongbak2025.shop",  # HTTP와 HTTPS 둘 다 추가
 
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000/",
+    "http://localhost:5173/",
+    "http://127.0.0.1:3000/",
+    "http://127.0.0.1:5173/",
 ]
